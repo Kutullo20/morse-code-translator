5c889466dd4a6fbf852f87ad644be6a3a1dde819
Kutullo Letageng <U21585513@tuks.co.za>
2025-05-30 11:53:21 +0200

    chore: add required using directives for form handling and data annotations in Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
 
 namespace morse_code_translator.Pages;
 
