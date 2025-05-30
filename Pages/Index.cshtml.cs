1668d2184e01da9b14d745af24d7243bae1542a0
Kutullo Letageng <U21585513@tuks.co.za>
2025-05-30 12:04:02 +0200

    feat: translator bindings and Morse/text conversion support

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -8,15 +8,80 @@ namespace morse_code_translator.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+    [BindProperty]
+    public MorseTranslatorModel Translator { get; set; } = new();
 
     public void OnGet()
     {
-
     }
+
+    public IActionResult OnPostTranslateToMorse()
+    {
+        if (ModelState.IsValid)
+        {
+            Translator.MorseResult = LettersToMorseCode(Translator.TextInput);
+        }
+        return Page();
+    }
+
+    public IActionResult OnPostTranslateToText()
+    {
+        if (ModelState.IsValid)
+        {
+            Translator.TextResult = MorseCodeToLetters(Translator.MorseInput);
+        }
+        return Page();
+    }
+        
+    private static readonly Dictionary<char, string> MorseCodeDictionary = new()
+        {
+            // Letters A-Z
+            {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."},
+            {'F', "..-."}, {'G', "--."}, {'H', "...."}, {'I', ".."}, {'J', ".---"},
+            {'K', "-.-"}, {'L', ".-.."}, {'M', "--"}, {'N', "-."}, {'O', "---"},
+            {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
+            {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"}, {'Y', "-.--"},
+            {'Z', "--.."},
+            
+            // Numbers 0-9
+            {'0', "-----"}, {'1', ".----"}, {'2', "..---"}, {'3', "...--"},
+            {'4', "....-"}, {'5', "....."}, {'6', "-...."}, {'7', "--..."}, 
+            {'8', "---.."}, {'9', "----."},
+            
+            // Punctuation and special characters
+            {'.', ".-.-.-"}, {',', "--..--"}, {'?', "..--.."}, {'\'', ".----."},
+            {'!', "-.-.--"}, {'/', "-..-."}, {'(', "-.--."}, {')', "-.--.-"}, 
+            {'&', ".-..."}, {':', "---..."}, {';', "-.-.-."}, {'=', "-...-"}, 
+            {'+', ".-.-."}, {'-', "-....-"}, {'_', "..--.-"}, {'"', ".-..-."}, 
+            {'$', "...-..-"}, {'@', ".--.-."},
+            
+            // Space character
+            {' ', "/"}
+        };
+
+        private static readonly Dictionary<string, char> ReverseMorseDictionary = new()
+        {
+            // Letters A-Z
+            {".-", 'A'}, {"-...", 'B'}, {"-.-.", 'C'}, {"-..", 'D'}, {".", 'E'},
+            {"..-.", 'F'}, {"--.", 'G'}, {"....", 'H'}, {"..", 'I'}, {".---", 'J'},
+            {"-.-", 'K'}, {".-..", 'L'}, {"--", 'M'}, {"-.", 'N'}, {"---", 'O'},
+            {".--.", 'P'}, {"--.-", 'Q'}, {".-.", 'R'}, {"...", 'S'}, {"-", 'T'},
+            {"..-", 'U'}, {"...-", 'V'}, {".--", 'W'}, {"-..-", 'X'}, {"-.--", 'Y'},
+            {"--..", 'Z'},
+            
+            // Numbers 0-9
+            {"-----", '0'}, {".----", '1'}, {"..---", '2'}, {"...--", '3'},
+            {"....-", '4'}, {".....", '5'}, {"-....", '6'}, {"--...", '7'},
+            {"---..", '8'}, {"----.", '9'},
+            
+            // Punctuation and special characters
+            {".-.-.-", '.'}, {"--..--", ','}, {"..--..", '?'}, {".----.", '\''},
+            {"-.-.--", '!'}, {"-..-.", '/'}, {"-.--.", '('}, {"-.--.-", ')'},
+            {".-...", '&'}, {"---...", ':'}, {"-.-.-.", ';'}, {"-...-", '='},
+            {".-.-.", '+'}, {"-....-", '-'}, {"..--.-", '_'}, {".-..-.", '"'},
+            {"...-..-", '$'}, {".--.-.", '@'},
+            
+            // Space character
+            {"/", ' '}
+        };
 }
